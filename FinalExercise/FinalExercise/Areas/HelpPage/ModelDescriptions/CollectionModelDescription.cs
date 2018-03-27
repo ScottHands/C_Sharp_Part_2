008e20bb0b272deff2ad79c1684ff1c875373a00
Scott Hands <sctyp1@gmail.com>
2018-03-27 10:39:46 -0700

    Delete CollectionModelDescription.cs

--- FinalExercise/FinalExercise/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace FinalExercise.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
